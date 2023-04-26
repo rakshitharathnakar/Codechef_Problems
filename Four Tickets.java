e1d89da2831f58c7c0e81bd47c10eebb89eb8184
Rakshitha Rathnakar <92865986+rakshitharathnakar@users.noreply.github.com>
2023-04-26 21:16:58 +0530

    Create Four Tickets.java

---
+++ Four Tickets.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    while(t-->0){
+	        int x=sc.nextInt();
+	        if(x*4<=1000)
+	           System.out.println("yes");
+	        else
+	           System.out.println("no");
+	    }
+	}
+}
