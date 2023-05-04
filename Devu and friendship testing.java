f65e710ee5a2194605d55c050e7aba933b5ba696
Rakshitha Rathnakar <92865986+rakshitharathnakar@users.noreply.github.com>
2023-05-04 23:31:53 +0530

    Create Devu and friendship testing.java

---
+++ Devu and friendship testing.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    {
+		    int n=sc.nextInt();
+		    int a[]=new int[101];
+		    int count=0;
+		    for(int i=0;i<n;i++)
+		    a[sc.nextInt()]++;
+		    for(int i=1;i<101;i++)
+		    {
+		        if(a[i]>0)
+		        count++;
+		    }
+		    System.out.println(count);
+		}
+		}
+	}
+}
