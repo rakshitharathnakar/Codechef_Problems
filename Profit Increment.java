4a88e1962493fe113a81d3fcac0e4f2b47114198
Rakshitha Rathnakar <92865986+rakshitharathnakar@users.noreply.github.com>
2023-05-06 22:57:15 +0530

    Create Profit Increment.java

---
+++ Profit Increment.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    int z=x/100*10+y;
+		    System.out.println(z);
+		}
+	}
+}
